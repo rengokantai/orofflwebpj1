15f8b5d9429bc1d0761dd13b3f441c90b2699026
Hernan Y.Ke <yuriqiao@gmail.com>
2016-01-25 14:03:30 -0500

    fetch api including respondWith and caches.match

--- CP3/serviceworker.js
+++ CP3/serviceworker.js
@@ -1,6 +1,19 @@
 /**
  * Created by Hernan Y.Ke on 2016/1/25.
  */
+var urls=['/projectroot','search.js'];
 self.addEventListener("install",function(e){
     console.log("installed");
+    e.waitUntil(caches.open("mycache").then(function(cache){
+        return cache.addAll(urls);
+    }));
+});
+self.addEventListener("fetch",function(event){
+    event.respondWith(caches.match(event.request)).then(function(response){
+        if(response){
+            return response;
+        }else{
+            return fetch(event.request);
+        }
+    })
 });
