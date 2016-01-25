cd5f864e3d6e694d6891ea61319e9bc682cb416a
Hernan Y.Ke <yuriqiao@gmail.com>
2016-01-25 16:01:32 -0500

    api change

--- CP3/serviceworker.js
+++ CP3/serviceworker.js
@@ -9,25 +9,23 @@ self.addEventListener("install",function(e){
     }));
 });
 self.addEventListener("fetch",function(event){
-    if(!navigator.onLine && event.request.url.indexOf("wiki.php")>0){
-        event.respondWith(new Response("offline"),
-            {
-                "headers":{
-                    "Content-Type":"text/html"
-                }
-            });
-    }else {
+    //if(!navigator.onLine && event.request.url.indexOf("wiki.php")>0){
+    //    event.respondWith(new Response("offline"),
+    //        {
+    //            "headers":{
+    //                "Content-Type":"text/html"
+    //            }
+    //        });
+    //}else {
         event.respondWith(caches.match(event.request)).then(function (response) {
-            if (response) {
-                return response;
-            } else {
-                return fetch(event.request).then(function(response){
-                    caches.open("myacache").then(function(cache){
-                        cache.put(event.request, response.clone());
-                        return response;
-                    })
-                });
-            }
-        });
-    }
+            //if (response) {
+            //    return response;
+            //} else {
+            var fetchPromise = fetch(event.request).then(function (networkResponse) {
+                //caches.open("myacache").then(function(cache){
+                cache.put(event.request, networkResponse.clone());
+                return networkResponse;
+            });
+            return response || fetchPromise;
+        })
 });
